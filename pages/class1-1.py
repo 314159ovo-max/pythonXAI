ef190d692d6104ce10eb2d6e54a1748b332899d0
314159ovo-max <314159ovo@gmail.com>
2026-07-13 16:40:49 +0800

    test1

---
+++ pages/class1-1.py
@@ -0,0 +1,18 @@
+print("hello world!!!")
+print("Tiffany")
+print("不要低頭\n雙下巴會出來")
+"""
+這是多行註解
+"""
+
+#這是單行註解
+print("hello world!!!")#print是在終端機顯示文字的指令
+#ctrl+?可以快速駐解/取消註解
+print(1)#int是整數(-1,0,1)
+print(1.0)#float是浮點數(1.0,3.14)
+print(1.234)#float是浮點數
+print("apple")#str是字串("sandy",'1')
+print(True)#bool是布林值(True,False)(開頭要大寫)
+print(False)#布林值(True,False)
+
+print()
