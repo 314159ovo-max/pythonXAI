eb39c828ca40e6380a2315df0dcc7c4e9ee78ad6
314159ovo-max <314159ovo@gmail.com>
2026-07-13 16:58:13 +0800

    Add variable assignments and arithmetic operations examples

--- pages/class1-1.py
+++ pages/class1-1.py
@@ -15,4 +15,34 @@ print("apple")#str是字串("sandy",'1')
 print(True)#bool是布林值(True,False)(開頭要大寫)
 print(False)#布林值(True,False)
 
+#變數
+a=10#新增一個儲存空間並取名為a,"="的功能是將右邊的值10存入左邊的a
+print(a)#在終端機顯示A所存在的值
+a="apple"#將a的值改為"apple"
+print(a)#在終端機顯示A所存的值
+
+#運算子
+print(1+1)#加法
+print(1-1)#減法
+print(2*3)#乘法
+print(6/2)#除法
+print(7//2)#取商
+print(7%2)#取餘數
+print(2**3)#次方
+
+# 優先順序
+# 1. () 括號
+# 2. ** 次方
+# 3. * / // % 乘 除 取商 取餘數
+# 4. + - 加 減
+
+#字串運算+-/*
+print("apple"+"pen")#字串相加
+print("apple"*3)#字串乘法
+
+# 字串格式化
+name = "apple"
+age = 18
+print(f"Hello, my name is {name}, I'm {age} years old.")  # f-string
+# 可以將變數或其他型態的資料放到f字串裡面的{}，這樣就可以在字串中顯示
 print()
